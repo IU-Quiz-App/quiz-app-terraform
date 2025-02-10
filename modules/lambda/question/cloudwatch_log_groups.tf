95f2729de2e277b0b6926fcef909d11fd6822b91
jannawitt <jannawitt@gmx.de>
2025-02-10 19:45:31 +0100

    Add Delete-question Lambda-functions, new lambda structure

--- modules/lambda/question/cloudwatch_log_groups.tf
+++ modules/lambda/question/cloudwatch_log_groups.tf
@@ -1,11 +1,17 @@
 resource "aws_cloudwatch_log_group" "get_question" {
-  name              = "/aws/lambda/${var.stage}/questions/get-question"
+  name              = "/aws/lambda/${var.stage}/question/get-question"
   log_group_class   = "STANDARD"
   retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "post_question" {
-  name              = "/aws/lambda/${var.stage}/questions/post-question"
+  name              = "/aws/lambda/${var.stage}/question/post-question"
+  log_group_class   = "STANDARD"
+  retention_in_days = 30
+}
+
+resource "aws_cloudwatch_log_group" "delete_question" {
+  name              = "/aws/lambda/${var.stage}/question/delete-question"
   log_group_class   = "STANDARD"
   retention_in_days = 30
 }
