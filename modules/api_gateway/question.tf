c5b3eb26c26b08437a08f3cf3384553d7192ba12
jannawitt <jannawitt@gmx.de>
2025-02-10 20:00:25 +0100

    API-Gateway Delete-question added, new API-Gateway structure

---
+++ modules/api_gateway/question.tf
@@ -0,0 +1,65 @@
+#get-question
+resource "aws_apigatewayv2_integration" "gateway_integration_question_get" {
+  api_id             = aws_apigatewayv2_api.api_gateway.id
+  integration_type   = "AWS_PROXY"
+  connection_type    = "INTERNET"
+  description        = "GET endpoint for questions"
+  integration_method = "POST"
+  integration_uri    = var.get_question_function_invoke_arn
+  response_parameters {
+    status_code = 403
+    mappings = {
+      "append:header.auth" = "$context.authorizer.authorizerResponse"
+    }
+  }
+}
+
+resource "aws_apigatewayv2_route" "get_question_route" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "GET /question"
+  target    = "integrations/${aws_apigatewayv2_integration.gateway_integration_question_get.id}"
+}
+
+#post-question
+resource "aws_apigatewayv2_integration" "gateway_integration_question_post" {
+  api_id             = aws_apigatewayv2_api.api_gateway.id
+  integration_type   = "AWS_PROXY"
+  connection_type    = "INTERNET"
+  description        = "POST endpoint for questions"
+  integration_method = "POST"
+  integration_uri    = var.post_question_function_invoke_arn
+  response_parameters {
+    status_code = 403
+    mappings = {
+      "append:header.auth" = "$context.authorizer.authorizerResponse"
+    }
+  }
+}
+
+resource "aws_apigatewayv2_route" "post_question_route" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "POST /question"
+  target    = "integrations/${aws_apigatewayv2_integration.gateway_integration_question_post.id}"
+}
+
+#delete-question
+resource "aws_apigatewayv2_integration" "gateway_integration_question_delete" {
+  api_id             = aws_apigatewayv2_api.api_gateway.id
+  integration_type   = "AWS_PROXY"
+  connection_type    = "INTERNET"
+  description        = "DELETE endpoint for questions"
+  integration_method = "POST"
+  integration_uri    = var.post_question_function_invoke_arn
+  response_parameters {
+    status_code = 403
+    mappings = {
+      "append:header.auth" = "$context.authorizer.authorizerResponse"
+    }
+  }
+}
+
+resource "aws_apigatewayv2_route" "delete_question_route" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "DELETE /question"
+  target    = "integrations/${aws_apigatewayv2_integration.gateway_integration_question_delete.id}"
+}
