d9bce9022cb247c913f8091e8d3e21607cdb78ed
jannawitt <jannawitt@gmx.de>
2025-03-31 14:20:21 +0200

    Lambda-function delete updated

--- lambda-code/question/delete-question.py
+++ lambda-code/question/delete-question.py
@@ -11,15 +11,14 @@ domain = os.environ.get('DOMAIN')
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(f"iu-quiz-questions-{stage}")
 
+CORS_HEADERS = {
+    "Access-Control-Allow-Origin": f"https://{domain}",
+    "Access-Control-Allow-Methods": "GET, OPTIONS, HEAD",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Credentials": "true"
+}
 
 def lambda_handler(event, context):
-    cors_headers = {
-        "Access-Control-Allow-Origin": "https://" + domain,
-        "Access-Control-Allow-Methods": "DELETE, OPTIONS, HEAD",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Credentials": "true"
-    }
-
     try:
         logger.info("Event: %s", event)
 
@@ -39,7 +38,7 @@ def lambda_handler(event, context):
 
         return {
             "statusCode": 200,
-            "headers": cors_headers,
+            "headers": CORS_HEADERS,
             "body": json.dumps({"message": "Question deleted successfully"})
         }
 
@@ -47,6 +46,6 @@ def lambda_handler(event, context):
         logger.error("Error deleting the question: %s", str(e), exc_info=True)
         return {
             "statusCode": 500,
-            "headers": cors_headers,
+            "headers": CORS_HEADERS,
             "body": json.dumps({"error": str(e)})
         }
