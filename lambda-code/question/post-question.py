b921e2590ce9c0c0b6021d59e8a7fdf1fecda076
Janschuri <124630782+Janschuri@users.noreply.github.com>
2025-04-13 14:41:01 +0200

    use token in post-question

--- lambda-code/question/post-question.py
+++ lambda-code/question/post-question.py
@@ -4,6 +4,7 @@ import uuid
 import datetime
 import logging
 import os
+import base64
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -22,6 +23,17 @@ CORS_HEADERS = {
 
 def lambda_handler(event, context):
     try:
+        token = event["headers"].get("authorization", "")
+        payload = decode_jwt_payload(token)
+
+        if not payload:
+            return http_response(401, {"error": "Invalid Token"})
+
+        user_id = payload.get("oid")
+
+        if not user_id:
+            return http_response(400, {"error": "Invalid Token: Missing oid"})
+
         body = json.loads(event["body"])
 
         for answer in body["answers"]:
@@ -32,7 +44,7 @@ def lambda_handler(event, context):
             "uuid": str(uuid.uuid4()),
             "text": body["text"],
             "answers": body["answers"],
-            "created_by": body["created_by"],
+            "created_by": user_id,
             "public": "true" if body.get("public", False) else "false",
             "status": body.get("status", "created"),
             "created_at": datetime.datetime.now().isoformat()
@@ -55,3 +67,26 @@ def lambda_handler(event, context):
             "headers": CORS_HEADERS,
             "body": json.dumps({"error": str(e)})
         }
+
+def http_response(status_code, body):
+    return {"statusCode": status_code, "headers": CORS_HEADERS, "body": json.dumps(body)}
+
+def decode_jwt_payload(token):
+    token = token.replace("Bearer ", "")
+    try:
+        parts = token.split('.')
+        if len(parts) != 3:
+            raise ValueError("Invalid JWT format")
+
+        payload_b64 = parts[1]
+        padding = '=' * (-len(payload_b64) % 4)
+        payload_b64 += padding
+
+        payload_bytes = base64.urlsafe_b64decode(payload_b64)
+        payload = json.loads(payload_bytes)
+
+        return payload
+
+    except Exception as e:
+        logger.error("Failed to decode JWT payload: %s", str(e))
+        return None
