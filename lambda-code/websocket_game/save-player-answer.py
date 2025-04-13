82d403fe0ef7f0575b509161ea538edc47557c0f
Janschuri <124630782+Janschuri@users.noreply.github.com>
2025-04-13 15:54:56 +0200

    use token in save-player-answer

--- lambda-code/websocket_game/save-player-answer.py
+++ lambda-code/websocket_game/save-player-answer.py
@@ -3,6 +3,7 @@ import boto3
 import datetime
 import logging
 import os
+import base64
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -11,30 +12,58 @@ stage = os.environ.get('STAGE')
 dynamodb = boto3.resource("dynamodb")
 game_answers_table = dynamodb.Table(f"iu-quiz-game-answers-{stage}")
 game_sessions_table = dynamodb.Table(f"iu-quiz-game-sessions-{stage}")
+websocket_connections_table = dynamodb.Table(f"websocket-connections-{stage}")
 
 def lambda_handler(event, context):
     logger.info(f"Received event: {json.dumps(event)}")
+
+    connection_uuid = event["requestContext"]["connectionId"]
+    connection = get_websocket_connection(connection_uuid)
+
+    if not connection:
+        logger.error(f"Connection {connection_uuid} not found")
+        return ws_response(400, {"error": "Connection not found"})
+
+    token = connection.get("access_token")
+    if not token:
+        logger.error("Missing access_token")
+        return ws_response(401, {"error": "Missing access_token"})
+
+    jwt_payload = decode_jwt_payload(token)
+    if not jwt_payload:
+        logger.error("Invalid Token")
+        return ws_response(401, {"error": "Invalid Token"})
+
+    user_uuid = jwt_payload.get("oid")
+
+    if not user_uuid:
+        logger.error("Invalid Token: Missing oid")
+        return ws_response(401, {"error": "Invalid Token: Missing oid"})
+
     body = json.loads(event["body"])
 
     game_session_uuid = body.get("game_session_uuid")
-    user_uuid = body.get("user_uuid")
     question_uuid = body.get("question_uuid")
     answer_uuid = body.get("answer_uuid")
     answered_at = datetime.datetime.now().isoformat()
 
     if not game_session_uuid:
-        return response(400, {"error": "Missing game_session_uuid"})
-    if not user_uuid:
-        return response(400, {"error": "Missing user_uuid"})
+        return ws_response(400, {"error": "Missing game_session_uuid"})
     if not question_uuid:
-        return response(400, {"error": "Missing question_uuid"})
+        return ws_response(400, {"error": "Missing question_uuid"})
     if not answer_uuid:
-        return response(400, {"error": "Missing answer_uuid"})
+        return ws_response(400, {"error": "Missing answer_uuid"})
     
     try:
+        game_session = game_sessions_table.get_item(Key={"uuid": game_session_uuid}).get("Item")
+
+        users = game_session.get("users", [])
+        if not any(user["user_uuid"] == user_uuid for user in users):
+            return ws_response(403, {"error": "User not in game session"})
+
         player_answers = get_player_answers(game_session_uuid, question_uuid)
         if not player_answers:
-            return response(404, {"error": "No player answers found"})
+            return ws_response(404, {"error": "No player answers found"})
         
         # Update the answer of the player
         for player_answer in player_answers:
@@ -59,14 +88,13 @@ def lambda_handler(event, context):
 
         # Send success token to step function if all players have answered
         if all_players_answered:
-            game_session = game_sessions_table.get_item(Key={"uuid": game_session_uuid}).get("Item")
             task_token = game_session.get("task_token")
             logger.info(f"Task token: {task_token}")
 #            task_token = game_sessions_table.get_item(Key={"uuid": game_session_uuid}, ProjectionExpression="task_token")
 #            logger.info(f"Task token: {task_token}")
             if not task_token:
-                return response(404, {"error": "Task token not found"})
-            
+                return ws_response(404, {"error": "Task token not found"})
+
             stepfunctions = boto3.client("stepfunctions")
             stepfunctions.send_task_success(
                 taskToken=task_token,
@@ -74,7 +102,7 @@ def lambda_handler(event, context):
             )
             logger.info(f"Task success sent to step function")
 
-        return response(200, {"message": "Answer saved successfully"})
+        return ws_response(200, {"message": "Answer saved successfully"})
 
 
 
@@ -82,7 +110,7 @@ def lambda_handler(event, context):
 
     except Exception as e:
         logger.error(f"Error: {str(e)}")
-        return response(500, {"error": str(e)})
+        return ws_response(500, {"error": str(e)})
 
 def get_player_answers(game_session_uuid, question_uuid):
     player_answers = game_answers_table.query(
@@ -96,5 +124,37 @@ def get_player_answers(game_session_uuid, question_uuid):
     logger.info(f"Player answers: {player_answers["Items"]}")
     return player_answers["Items"]
 
-def response(status_code, body):
+def decode_jwt_payload(token):
+    token = token.replace("Bearer ", "")
+    try:
+        parts = token.split('.')
+        if len(parts) != 3:
+            raise ValueError("Invalid JWT format")
+
+        payload_b64 = parts[1]
+        padding = '=' * (-len(payload_b64) % 4)
+        payload_b64 += padding
+
+        payload_bytes = base64.urlsafe_b64decode(payload_b64)
+        payload = json.loads(payload_bytes)
+
+        return payload
+
+    except Exception as e:
+        logger.error("Failed to decode JWT payload: %s", str(e))
+        return None
+
+def ws_response(status_code, body):
     return {"statusCode": status_code, "body": body}
+
+def get_websocket_connection(connection_id):
+    try:
+        response = websocket_connections_table.get_item(
+            Key={
+                "connection_uuid": connection_id
+            }
+        )
+        return response.get("Item")
+    except Exception as e:
+        logger.error(f"Error getting websocket connection: {str(e)}")
+        return None
