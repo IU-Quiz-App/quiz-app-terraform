ee77ef97a0f9e48bb21de828882facd549de1947
Janschuri <124630782+Janschuri@users.noreply.github.com>
2025-04-13 14:43:50 +0200

    use token in get-questions

--- lambda-code/question/get-questions.py
+++ lambda-code/question/get-questions.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import logging
 import os
+import base64
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -22,7 +23,17 @@ def lambda_handler(event, context):
     try:
         logger.info("Event: %s", event)
 
-        user_id = event["queryStringParameters"].get("user_id")
+        token = event["headers"].get("authorization", "")
+        payload = decode_jwt_payload(token)
+
+        if not payload:
+            return http_response(401, {"error": "Invalid Token"})
+
+        user_id = payload.get("oid")
+
+        if not user_id:
+            return http_response(400, {"error": "Invalid Token: Missing oid"})
+
         page = int(event["queryStringParameters"].get("page", 1))
         page_size = int(event["queryStringParameters"].get("page_size", 10))
 
@@ -85,3 +96,27 @@ def lambda_handler(event, context):
             "headers": CORS_HEADERS,
             "body": json.dumps({"error": str(e)})
         }
+
+
+def http_response(status_code, body):
+    return {"statusCode": status_code, "headers": CORS_HEADERS, "body": json.dumps(body)}
+
+def decode_jwt_payload(token):
+    token = token.replace("Bearer ", "")
+    try:
+        parts = token.split('.')
+        if len(parts) != 3:
+            raise ValueError("Invalid JWT format")
+
+        payload_b64 = parts[1]
+        padding = '=' * (-len(payload_b64) % 4)
+        payload_b64 += padding
+
+        payload_bytes = base64.urlsafe_b64decode(payload_b64)
+        payload = json.loads(payload_bytes)
+
+        return payload
+
+    except Exception as e:
+        logger.error("Failed to decode JWT payload: %s", str(e))
+        return None
