e920faa8cf322fd4a79264ef8753cc3ec51c6a11
DevOpsPH <philipp-hoyer@web.de>
2025-04-24 23:21:37 +0200

    remove unused resources

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -70,8 +70,8 @@ resource "aws_apigatewayv2_api" "websocket_api_gateway" {
 }
 
 resource "aws_apigatewayv2_stage" "websocket_api_stage" {
-  depends_on = [aws_api_gateway_account.websocket_api_gateway_account]
-  api_id     = aws_apigatewayv2_api.websocket_api_gateway.id
+  #depends_on = [aws_api_gateway_account.websocket_api_gateway_account]
+  api_id = aws_apigatewayv2_api.websocket_api_gateway.id
 
   name        = var.stage
   auto_deploy = true
@@ -131,56 +131,4 @@ resource "aws_apigatewayv2_api_mapping" "websocket_api_mapping" {
   api_id      = aws_apigatewayv2_api.websocket_api_gateway.id
   domain_name = aws_apigatewayv2_domain_name.websocket_domain_name.domain_name
   stage       = var.stage
-}
-
-resource "aws_api_gateway_account" "websocket_api_gateway_account" {
-  cloudwatch_role_arn = aws_iam_role.api_gateway_logs_role.arn
-}
-
-# Roles and Policies for logging
-resource "aws_iam_role" "api_gateway_logs_role" {
-  name = "api_gateway_logs_role_${var.stage}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = ["apigateway.amazonaws.com", "lambda.amazonaws.com"]
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_policy" "api_gateway_logs_policy" {
-  name        = "api_gateway_logs_policy_${var.stage}"
-  description = "Policy to allow API Gateway to write to CloudWatch Logs"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams",
-          "logs:PutLogEvents",
-          "logs:GetLogEvents",
-          "logs:FilterLogEvents"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "api_gateway_logs_policy_attachment" {
-  name       = "api-gateway-logs-policy-attachment"
-  policy_arn = aws_iam_policy.api_gateway_logs_policy.arn
-  roles      = [aws_iam_role.api_gateway_logs_role.name]
 }
